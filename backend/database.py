382d0d383476ce9ba2a948c43e60986da9320a8c
Rabeesha Batool <batoolrabeesha@gmail.cm>
2025-11-22 18:51:21 +0500

    Hardcode MongoDB URI for Vercel debugging

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,7 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
+# WARNING: Hardcoded credentials. DO NOT use in production if repo is public.
+MONGO_URL = "mongodb+srv://rizwanjaliawala_db_user:2wbVTBj7XayiI2AU@tms.ddmz06v.mongodb.net/?retryWrites=true&w=majority"
 client = AsyncIOMotorClient(MONGO_URL, serverSelectionTimeoutMS=5000)
 db = client.Trans
