94839a239c9cf8b03ca6ea79d34658412a2f873a
Rabeesha Batool <batoolrabeesha@gmail.cm>
2025-11-22 18:56:37 +0500

    Update MongoDB password with correct credentials

--- backend/database.py
+++ backend/database.py
@@ -2,6 +2,6 @@ from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
 # WARNING: Hardcoded credentials. DO NOT use in production if repo is public.
-MONGO_URL = "mongodb+srv://rizwanjaliawala_db_user:2wbVTBj7XayiI2AU@tms.ddmz06v.mongodb.net/?retryWrites=true&w=majority"
+MONGO_URL = "mongodb+srv://rizwanjaliawala_db_user:Rabeesha1209.@tms.ddmz06v.mongodb.net/?retryWrites=true&w=majority"
 client = AsyncIOMotorClient(MONGO_URL, serverSelectionTimeoutMS=5000)
 db = client.Trans
